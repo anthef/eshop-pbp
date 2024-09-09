cc3f028d9e4c36ce0c4cca00e7474ba042077edf
anthef <anthonyef09@gmail.com>
2024-09-09 11:24:19 +0700

    mengisi readme

--- eshop_pbp/urls.py
+++ eshop_pbp/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+from main import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
+    path('',views.show_main,name='show_main'),
 ]
