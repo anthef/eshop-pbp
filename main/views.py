a11b131fe85df1b79d3d4c0af2399561edb69de4
anthef <anthonyef09@gmail.com>
2024-09-10 17:30:53 +0700

    adjustment on views

--- main/views.py
+++ main/views.py
@@ -10,12 +10,31 @@ def show_main(request):
         if product.name not in seen:
             unique_products.append(product)
             seen.add(product.name)
+    
+    temporary = [
+        {
+            'name':"Laptop", 
+            'price':1200, 
+            'description':"A high-performance laptop."
+        },
+        {
+            'name':"Smartphone", 
+            'price':1000, 
+            'description':"A latest model smartphone."
+        },
+        {
+            'name':"Computer", 
+            'price':1000, 
+            'description':"A latest model computer."
+        }
+    ]
 
     context = {
         'npm': '2306165654',
         'name': 'Anthony Edbert Feriyanto',
-        'class_name': 'PBP C',  
-        'products': unique_products  
+        'class_name': 'PBP C',
+        'shop_name':'Ayo Belanja',  
+        'products': temporary  
     }
 
     return render(request, "main.html", context)
