47ebced9911d1d8a7c2ad587ce5f36e5e27d0ace
anthef <anthonyef09@gmail.com>
2024-09-10 17:36:30 +0700

    show product

--- main/management/commands/add_products.py
+++ main/management/commands/add_products.py
@@ -1,17 +1,17 @@
-from django.core.management.base import BaseCommand
-from main.models import ProductEntry
+# from django.core.management.base import BaseCommand
+# from main.models import ProductEntry
 
-class Command(BaseCommand):
-    help = 'Add sample products to the database'
+# class Command(BaseCommand):
+#     help = 'Add sample products to the database'
 
-    def handle(self, *args, **kwargs):
-        product1 = ProductEntry(name="Laptop", price=1200, description="A high-performance laptop.")
-        product1.save()
+#     def handle(self, *args, **kwargs):
+#         product1 = ProductEntry(name="Laptop", price=1200, description="A high-performance laptop.")
+#         product1.save()
 
-        product2 = ProductEntry(name="Smartphone", price=800, description="A latest model smartphone.")
-        product2.save()
+#         product2 = ProductEntry(name="Smartphone", price=800, description="A latest model smartphone.")
+#         product2.save()
 
-        product3 = ProductEntry(name="Computer", price=1000, description="A latest model computer.")
-        product3.save()
+#         product3 = ProductEntry(name="Computer", price=1000, description="A latest model computer.")
+#         product3.save()
 
-        self.stdout.write(self.style.SUCCESS('Successfully added products'))
+#         self.stdout.write(self.style.SUCCESS('Successfully added products'))
