8d925b910160e0bbe58880d04d0c045c9048c53e
anthef <anthonyef09@gmail.com>
2024-09-15 09:41:30 +0700

    finish tugas 2 without readme

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from main.views import show_main
+from main.views import show_main,create_name_entry,show_xml,show_json,show_xml_by_id,show_json_by_id,delete_product_entry
 
 app_name = 'main'
 
 urlpatterns = [
     path('', show_main, name='show_main'),
+    path('create-name-entry', create_name_entry, name='create_name_entry'),
+    path('xml/',show_xml,name='show_xml'),
+    path('json/',show_json,name='show_json'),
+    path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
+    path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
+    path('delete/<uuid:id>/', delete_product_entry, name='delete_product_entry'),
 ]
--- main/views.py
+++ main/views.py
@@ -1,40 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from .models import ProductEntry  
+from main.forms import ProductEntryForm
+from django.http import HttpResponse
+from django.core import serializers
+
 
 def show_main(request):
     products = ProductEntry.objects.all()
-
-    seen = set()
-    unique_products = []
-    for product in products:
-        if product.name not in seen:
-            unique_products.append(product)
-            seen.add(product.name)
-    
-    temporary = [
-        {
-            'name':"Laptop", 
-            'price':1200, 
-            'description':"A high-performance laptop."
-        },
-        {
-            'name':"Smartphone", 
-            'price':1000, 
-            'description':"A latest model smartphone."
-        },
-        {
-            'name':"Computer", 
-            'price':1000, 
-            'description':"A latest model computer."
-        }
-    ]
 
     context = {
         'npm': '2306165654',
         'name': 'Anthony Edbert Feriyanto',
         'class_name': 'PBP C',
         'shop_name':'Ayo Belanja',  
-        'products': temporary  
+        'product_entry' : products
     }
 
     return render(request, "main.html", context)
+
+def create_name_entry(request):
+    form = ProductEntryForm(request.POST or None)
+    if form.is_valid() and request.method == 'POST':
+        form.save()
+        return redirect('main:show_main')
+    context={'form':form}
+    return render(request,'create_name_entry.html',context)
+
+
+def delete_product_entry(request, id):
+    if request.method == 'POST':
+        product = get_object_or_404(ProductEntry, id=id)
+        product.delete()
+        return redirect('main:show_main')
+    return redirect('main:show_main')
+
+
+def show_xml(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("xml",data),content_type='application/xml')
+
+def show_json(request):
+    data = ProductEntry.objects.all()
+    return HttpResponse(serializers.serialize("json",data),content_type='application/json')
+
+def show_xml_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("xml", data), content_type="application/xml")
+
+def show_json_by_id(request, id):
+    data = ProductEntry.objects.filter(pk=id)
+    return HttpResponse(serializers.serialize("json", data), content_type="application/json")
