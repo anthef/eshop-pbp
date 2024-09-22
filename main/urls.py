b54a444d256c446c42ddc2dc6823423df194dbb2
anthef <anthonyef09@gmail.com>
2024-09-22 20:01:35 +0700

    finish the code for tugas 4

--- main/urls.py
+++ main/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from main.views import show_main,create_name_entry,show_xml,show_json,show_xml_by_id,show_json_by_id,delete_product_entry
+from main.views import show_main,create_name_entry,show_xml,show_json,show_xml_by_id,show_json_by_id,delete_product_entry,register,login_user,logout_user
 
 app_name = 'main'
 
@@ -11,4 +11,8 @@ urlpatterns = [
     path('xml/<str:id>/', show_xml_by_id, name='show_xml_by_id'),
     path('json/<str:id>/', show_json_by_id, name='show_json_by_id'),
     path('delete/<uuid:id>/', delete_product_entry, name='delete_product_entry'),
+    path('register/', register, name='register'),
+    path('login/', login_user, name='login'),
+    path('logout/', logout_user, name='logout_user'),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,19 +1,26 @@
 from django.shortcuts import render,redirect,get_object_or_404
 from .models import ProductEntry  
 from main.forms import ProductEntryForm
-from django.http import HttpResponse
+from django.http import HttpResponse,HttpResponseRedirect
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm,AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login,logout
+from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+import datetime
 
-
+@login_required(login_url='/login')
 def show_main(request):
-    products = ProductEntry.objects.all()
+    products = ProductEntry.objects.filter(user=request.user)
 
     context = {
         'npm': '2306165654',
-        'name': 'Anthony Edbert Feriyanto',
+        'name': request.user.username,
         'class_name': 'PBP C',
         'shop_name':'Ayo Belanja',  
-        'product_entry' : products
+        'product_entry' : products,
+        'last_login': request.COOKIES['last_login'],
     }
 
     return render(request, "main.html", context)
@@ -21,7 +28,9 @@ def show_main(request):
 def create_name_entry(request):
     form = ProductEntryForm(request.POST or None)
     if form.is_valid() and request.method == 'POST':
-        form.save()
+        name_entry = form.save(commit=False)
+        name_entry.user = request.user
+        name_entry.save()
         return redirect('main:show_main')
     context={'form':form}
     return render(request,'create_name_entry.html',context)
@@ -50,3 +59,41 @@ def show_xml_by_id(request, id):
 def show_json_by_id(request, id):
     data = ProductEntry.objects.filter(pk=id)
     return HttpResponse(serializers.serialize("json", data), content_type="application/json")
+
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()  
+            if user is not None:
+                login(request, user)
+                response = HttpResponseRedirect(reverse("main:show_main"))
+                response.set_cookie('last_login', str(datetime.datetime.now()))
+                return response
+    else:
+        form = AuthenticationForm()
+    
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    print("Logout successful, redirecting to login page") 
+    return response
+
+
+
