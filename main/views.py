f15df8463df35321624343309a79c4d69b02c337
anthef <anthonyef09@gmail.com>
2024-10-29 23:06:59 +0700

    add decorator

--- main/views.py
+++ main/views.py
@@ -10,9 +10,9 @@ from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 import datetime
 from django.views.decorators.csrf import csrf_exempt
-from django.views.decorators.http import require_POST
 from django.utils.html import strip_tags
 import json
+from django.views.decorators.http import require_POST
 
 @login_required(login_url='/login')
 def show_main(request):
